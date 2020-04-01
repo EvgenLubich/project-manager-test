71da0f61a9a96720c9563c94ba537bac935e413d
kidman <kidman@syneforge.com>
2020-04-01 16:44:19 +0300

    1:07

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -1,25 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kidman
- * Date: 31.03.20
- * Time: 17:19
- */
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-/**
- * @Route("/", name="home")
- * @return Response
- */
-    public function index(): Response
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index(Request $request): Response
     {
         return $this->render('app/home.html.twig');
     }
