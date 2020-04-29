1ea3640e873dcce33cf60cf359e9f5b999bae389
kidman <kidman@syneforge.com>
2020-04-29 12:26:40 +0300

    Added network auth

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -7,7 +7,7 @@ use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\User;
 use App\Model\User\Entity\User\UserRepository;
-use App\Model\User\Service\ConfirmTokenizer;
+use App\Model\User\Service\SignUpConfirmTokenizer;
 use App\Model\User\Service\ConfirmTokenSender;
 use App\Model\User\Service\PasswordHasher;
 
@@ -19,7 +19,13 @@ class Handler
     private $sender;
     private $flusher;
 
-    public function __construct(UserRepository $users, PasswordHasher $hasher, ConfirmTokenizer $tokenizer, ConfirmTokenSender $sender, Flusher $flusher)
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        SignUpConfirmTokenizer $tokenizer,
+        ConfirmTokenSender $sender,
+        Flusher $flusher
+    )
     {
         $this->users = $users;
         $this->hasher = $hasher;
@@ -31,13 +37,17 @@ class Handler
     public function handle(Command $command): void
     {
         $email = new Email($command->email);
+
         if ($this->users->hasByEmail($email)) {
             throw new \DomainException('User already exists');
         }
 
         $user = new User(
             Id::next(),
-            new \DateTimeImmutable(),
+            new \DateTimeImmutable()
+        );
+
+        $user->signUpByEmail(
             $email,
             $this->hasher->hash($command->password),
             $token = $this->tokenizer->generate()
@@ -46,6 +56,7 @@ class Handler
         $this->sender->send($email, $token);
 
         $this->users->add($user);
+
         $this->flusher->flush();
     }
 }
